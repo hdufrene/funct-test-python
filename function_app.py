87a55e75eaf0958333c4ecea7b17ef978ad7a2e5
hdufrene <64784039+hdufrene@users.noreply.github.com>
2023-09-27 14:52:23 +0200

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,5 +1,5 @@
-@app.function_name(name="HttpTrigger1")
-@app.route(route="hello")
+@app.function_name(name="function1")
+
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
      logging.info('Python HTTP trigger function processed a request.')
 
